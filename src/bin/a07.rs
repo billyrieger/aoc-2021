b6c844537732f8be66be274f040d68b71c61996e
Billy Rieger <wrieger@protonmail.com>
2021-12-08 10:21:41 -0500

    Day 07

--- src/bin/a07.rs
+++ src/bin/a07.rs
@@ -1,1 +1,21 @@
-fn main() {}
+use itertools::Itertools;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = std::fs::read_to_string("input/07.txt")?;
+    let positions: Vec<i32> = file.trim().split(',').map(str::parse).try_collect()?;
+    let &min = positions.iter().min().unwrap();
+    let &max = positions.iter().max().unwrap();
+    let fuel1 = |delta: i32| delta.abs();
+    let fuel2 = |delta: i32| delta.abs() * (delta.abs() + 1) / 2;
+    let fuel_used: i32 = (min..=max)
+        .map(|i| positions.iter().map(|pos| fuel1(pos - i)).sum())
+        .min()
+        .unwrap();
+    println!("1: {}", fuel_used);
+    let fuel_used: i32 = (min..=max)
+        .map(|i| positions.iter().map(|pos| fuel2(pos - i)).sum())
+        .min()
+        .unwrap();
+    println!("2: {}", fuel_used);
+    Ok(())
+}
