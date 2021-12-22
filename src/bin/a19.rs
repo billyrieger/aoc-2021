c7427a36e646ca6cb6b164ec1c5475870fb3ab8c
Billy Rieger <wrieger@protonmail.com>
2021-12-22 01:16:27 -0500

    Days 19 and 21 (gross!!!)

--- src/bin/a19.rs
+++ src/bin/a19.rs
@@ -1,1 +1,136 @@
-fn main() {}
+use std::collections::{HashMap, HashSet, VecDeque};
+
+use aoc_2021::Itertools;
+use itertools::iproduct;
+
+const INPUT: &str = include_str!("../../input/19.txt");
+
+type Point = [i32; 3];
+
+struct Scanner {
+    beacons: HashMap<Point, HashSet<i32>>,
+}
+
+impl Scanner {
+    fn new(input: Vec<Point>) -> Option<Self> {
+        let mut beacons: HashMap<_, _> = input.iter().map(|&p| (p, HashSet::new())).collect();
+        for (&u, &v) in input.iter().tuple_combinations() {
+            let dist = norm_sq(u, v);
+            beacons.get_mut(&u)?.insert(dist);
+            beacons.get_mut(&v)?.insert(dist);
+        }
+        Self { beacons }.into()
+    }
+
+    fn merge(&mut self, other: &Self) -> Option<Point> {
+        let mut common: Vec<(Point, Point)> = vec![];
+        for ((&u, dists0), (&v, dists1)) in iproduct!(&self.beacons, &other.beacons) {
+            let similarity = dists0.iter().filter(|d| dists1.contains(d)).count();
+            if similarity >= 11 {
+                common.push((u, v));
+            }
+        }
+        if common.len() < 11 {
+            return None;
+        }
+        type Func = Box<dyn Fn(Point) -> Point>;
+        let orientations: Vec<Func> = iproduct!([-1, 1], [-1, 1], [-1, 1])
+            .flat_map(|(i, j, k)| {
+                [
+                    Box::new(move |[x, y, z]: Point| [i * x, j * y, k * z]) as Func,
+                    Box::new(move |[x, y, z]: Point| [i * x, j * z, k * y]) as Func,
+                    Box::new(move |[x, y, z]: Point| [i * y, j * x, k * z]) as Func,
+                    Box::new(move |[x, y, z]: Point| [i * y, j * z, k * x]) as Func,
+                    Box::new(move |[x, y, z]: Point| [i * z, j * x, k * y]) as Func,
+                    Box::new(move |[x, y, z]: Point| [i * z, j * y, k * x]) as Func,
+                ]
+            })
+            .collect();
+
+        for func in orientations {
+            if common
+                .iter()
+                .copied()
+                .map(|(u, v)| (u, func(v)))
+                .map(|(u, v)| [v[0] - u[0], v[1] - u[1], v[2] - u[2]])
+                .all_equal()
+            {
+                let (u, mut v) = common[0];
+                v = func(v);
+                let delta: Point = [u[0] - v[0], u[1] - v[1], u[2] - v[2]];
+                for (&point, dists) in &other.beacons {
+                    let mut new = func(point);
+                    new = [new[0] + delta[0], new[1] + delta[1], new[2] + delta[2]];
+                    self.beacons
+                        .entry(new)
+                        .or_insert_with(|| HashSet::new())
+                        .extend(dists);
+                }
+                let mut s = func([0, 0, 0]);
+                s = [s[0] + delta[0], s[1] + delta[1], s[2] + delta[2]];
+                return Some(s);
+            }
+        }
+
+        None
+    }
+}
+
+fn norm_sq(u: Point, v: Point) -> i32 {
+    u.into_iter().zip(v).map(|(i, j)| (i - j).pow(2)).sum()
+}
+
+fn solve() -> Option<()> {
+    let mut scanners: Vec<Scanner> = INPUT
+        .lines()
+        .group_by(|line| line.is_empty())
+        .into_iter()
+        // skip the empty lines between scanners
+        .step_by(2)
+        .map(|(_, group)| {
+            Scanner::new(
+                group
+                    // skip the header line
+                    .skip(1)
+                    .map(|line| {
+                        // parse each digit
+                        let (x, y, z) = line
+                            .split(',')
+                            .map(|num| num.parse().ok())
+                            .collect_tuple()?;
+                        [x?, y?, z?].into()
+                    })
+                    .collect::<Option<_>>()?,
+            )
+        })
+        .collect::<Option<_>>()?;
+
+    let (all, others) = scanners.split_first_mut()?;
+    let mut queue = VecDeque::from_iter(others);
+    let mut centers = vec![[0; 3]];
+    while let Some(scanner) = queue.pop_front() {
+        if let Some(center) = all.merge(&scanner) {
+            centers.push(center);
+        } else {
+            queue.push_back(scanner);
+        }
+    }
+    println!("1: {}", all.beacons.len());
+    println!(
+        "2: {}",
+        centers
+            .into_iter()
+            .tuple_combinations()
+            .map(|(p, q)| p
+                .iter()
+                .zip(q.iter())
+                .map(|(x0, x1)| (x0 - x1).abs())
+                .sum::<i32>())
+            .max()?
+    );
+    Some(())
+}
+
+fn main() {
+    solve().unwrap();
+}
--- src/bin/a21.rs
+++ src/bin/a21.rs
@@ -1,1 +1,98 @@
-fn main() {}
+use aoc_2021::Itertools;
+
+const INPUT: &str = include_str!("../../input/21.txt");
+
+fn solve() -> Option<()> {
+    let (p1, p2): (Option<i64>, Option<i64>) = INPUT
+        .lines()
+        .map(|line| line.split_once(':')?.1.trim().parse().ok())
+        .collect_tuple()?;
+    let (p0, p1) = (p1?, p2?);
+    part1(p0, p1)?;
+
+    dbg!(Universe::new(p0, p1).count_winning());
+    Some(())
+}
+
+#[derive(Clone, Copy)]
+struct Universe {
+    rolls: usize,
+    positions: [i64; 2],
+    scores: [i64; 2],
+    player: usize,
+    threshold: i64,
+}
+
+impl Universe {
+    fn new(p0: i64, p1: i64) -> Self {
+        Self {
+            rolls: 0,
+            positions: [p0, p1],
+            scores: [0, 0],
+            player: 0,
+            threshold: 21,
+        }
+    }
+
+    fn step(&mut self, sum: i64) {
+        self.rolls += 3;
+        self.positions[self.player] = (self.positions[self.player] + sum - 1) % 10 + 1;
+        self.scores[self.player] += self.positions[self.player];
+        self.player = 1 - self.player;
+    }
+
+    fn count_winning(&self) -> (i64, i64) {
+        let mut wins = (0, 0);
+        let stats = [[3, 1], [4, 3], [5, 6], [6, 7], [7, 6], [8, 3], [9, 1]];
+        for [sum, freq] in stats {
+            let mut universe = self.clone();
+            universe.step(sum);
+            if universe.scores[0] >= self.threshold {
+                wins.0 += freq;
+            } else if universe.scores[1] >= self.threshold {
+                wins.1 += freq;
+            } else {
+                let new_wins = universe.count_winning();
+                wins.0 += freq * new_wins.0;
+                wins.1 += freq * new_wins.1;
+            }
+        }
+        wins
+    }
+}
+
+fn part1(mut p0: i64, mut p1: i64) -> Option<()> {
+    let (mut score0, mut score1) = (0, 0);
+    let mut dice = (1..=100).cycle();
+    let threshold = 1000;
+    let mut rolls = 0;
+    loop {
+        // player one rolls dice
+        let (d0, d1, d2) = dice.next_tuple()?;
+        rolls += 3;
+        p0 = (p0 + d0 + d1 + d2 - 1) % 10 + 1;
+        score0 += p0;
+        if score0 >= threshold {
+            // player 2 lost
+            println!("1: {}", score1 * rolls);
+            break;
+        }
+
+        // player two rolls dice
+        let (d0, d1, d2) = dice.next_tuple()?;
+        rolls += 3;
+        p1 = (p1 + d0 + d1 + d2 - 1) % 10 + 1;
+        score1 += p1;
+        if score1 >= threshold {
+            // player 1 lost
+            println!("1: {}", score0 * rolls);
+            break;
+        }
+    }
+
+    Some(())
+}
+
+fn main() {
+    solve().unwrap();
+}
